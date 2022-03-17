c7dd01673cc0358f343793905ea0d3d4253bd608
p22cobrien <p22cobrien@fairfieldprep.org>
2022-03-17 10:20:01 -0400

    wip

--- word_play-2.py
+++ word_play-2.py
@@ -1,6 +1,5 @@
 # Author: CMOB 3/16/2022
 
-from unittest.util import _count_diff_all_purpose
 
 
 words = open("/Users/p22cobrien/Desktop/Case Studies/case-study-word-play-p22cobrien/words.txt","r")
@@ -8,17 +7,20 @@ out_file = open("/Users/p22cobrien/Desktop/Case Studies/case-study-word-play-p22
 
 def no_e():
     total = 0
-    no_e = 0
+    e = 0
+    
     while True:
         line = words.readline()
-        total += 1
         if "e" not in line:
-            out_file.write(line)
-            no_e += 1
-            continue
+            words.write(line)
+            total += 1
+            e += 1
+        elif "e" in line:
+            total += 1
         if len(line) == 0:
-            perc = no_e / total
             break
+    percentage = (e / total) * 100
+    print(percentage)
 
 no_e()
 
